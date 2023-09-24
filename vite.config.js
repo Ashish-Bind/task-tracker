20aa963ca7a150aa676bc568aa667f7da84aa7b1
Ashish <officialsandwork@gmail.com>
2023-09-24 12:46:26 +0530

    feat: PWA Functionality

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Task Tracker App',
+        short_name: 'Task Tracker App',
+        description: 'A progressive web app built with React and Vite',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/done.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+    }),
+  ],
 })
